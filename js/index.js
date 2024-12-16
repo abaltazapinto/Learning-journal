1e299c87a2e96006e7298b3bf78692f5e0986c86
andre <andrebaltazarpinto@gmail.com>
2024-12-16 10:31:33 +0000

    light dark mode

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 // Handle dark mode toggle
+
+// Handle dark mode toggle
 const toggle = document.getElementById("darkmode-toggle");
-console.log(toggle);
-console.log("afdsuihdsa");
-// toggle.setAttribute(
-//   "aria-label",
-//   "Toggle between the two theme possibles dark and light",
-// );
+toggle.setAttribute(
+  "aria-label",
+  "Toggle between the two theme possibles dark and light",
+);
+
 toggle.addEventListener("change", function () {
     document.body.classList.toggle("dark-mode", toggle.checked);
   });
